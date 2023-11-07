6724e03696ef1ff5885936c6f081d18962c1482e
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2023-11-07 16:32:38 +0100

    malianteo

---
+++ ModulePlayer.cpp
@@ -0,0 +1,61 @@
+﻿#include "Globals.h"
+#include "Application.h"
+#include "ModulePlayer.h"
+#include "ModuleInput.h"
+#include "ModuleSceneIntro.h"
+#include "ModulePhysics.h"
+#include "SDL\include\SDL.h"
+
+ModulePlayer::ModulePlayer(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModulePlayer::~ModulePlayer()
+{}
+
+// Load assets
+bool ModulePlayer::Start()
+{
+	LOG("Loading player");
+	return true;
+}
+
+// Unload assets
+bool ModulePlayer::CleanUp()
+{
+	LOG("Unloading player");
+
+	return true;
+}
+
+void ModulePlayer::moveFlipFlops() {
+
+	if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_DOWN) {
+
+		App->scene_intro->flipFlopLeft->body->ApplyForce({ 10,80 }, { 0,0 }, true);
+
+		LOG("Лорем Ипсум");
+	}
+
+	if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_DOWN) {
+		LOG("ole la derecha ARRIBA ESPAÑIA");
+	}
+
+}
+
+void ModulePlayer::launchBall() {
+
+	if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN) {
+		LOG("Pulsasto espacio ermano");
+	}
+
+}
+
+// Update: draw background
+update_status ModulePlayer::Update()
+{
+	return UPDATE_CONTINUE;
+}
+
+
+
---
+++ ModuleSceneIntro.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Module.h"
+#include "p2List.h"
+#include "p2Point.h"
+#include "Globals.h"
+#include "ModulePlayer.h"
+#include "SDL/include/SDL_render.h"
+
+class PhysBody;
+
+class ModuleSceneIntro : public Module
+{
+public:
+	ModuleSceneIntro(Application* app, bool start_enabled = true);
+	~ModuleSceneIntro();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+	void OnCollision(PhysBody* bodyA, PhysBody* bodyB);
+
+public:
+	p2List<PhysBody*> circles;
+	p2List<PhysBody*> boxes;
+	p2List<PhysBody*> ricks;
+
+	ModulePlayer* player;
+
+	PhysBody* sensor;
+	bool sensed;
+
+	SDL_Texture* circle;
+	SDL_Texture* box;
+	SDL_Texture* rick;
+
+	PhysBody* flipFlopLeft;
+	PhysBody* flipFlopRight;
+
+	uint bonus_fx;
+	p2Point<int> ray;
+	bool ray_on;
+};
